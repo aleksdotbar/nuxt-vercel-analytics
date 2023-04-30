3d8450b632466be6aa93cb7ed0a2fa2c3899d775
Alexander B <mail@xanderbarkhatov.com>
2023-04-30 20:03:27 +0600

    fix: add config types

--- src/module.ts
+++ src/module.ts
@@ -6,34 +6,11 @@ import {
 } from "@nuxt/kit";
 import type { inject } from "@vercel/analytics";
 
-type AnalyticsProps = Exclude<Parameters<typeof inject>[0], undefined>;
-
-interface ModuleOptions {
-  /**
-   * Override the automatic environment detection.
-   * This option allows you to force a specific environment for the package.
-   * @default 'auto'
-   */
-  mode?: AnalyticsProps["mode"];
-
-  /**
-   * You'll see all analytics events in the browser's console with the debug mode.
-   * This option is automatically enabled if the NODE_ENV environment variable is available and is either development or test.
-   */
-  debug?: AnalyticsProps["debug"];
-
-  /**
-   * With the beforeSend option, you can modify the event data before it's sent to Vercel.
-   * Returning null will ignore the event and no data will be sent.
-   */
-  beforeSend?: AnalyticsProps["beforeSend"];
-}
-
 export default defineNuxtModule({
   meta: {
     name: "nuxt-vercel-analytics",
   },
-  setup(_, nuxt) {
+  setup() {
     const { resolve } = createResolver(import.meta.url);
 
     addPlugin({
@@ -46,13 +23,32 @@ export default defineNuxtModule({
       name: "track",
       as: "vercelTrack",
     });
-
-    nuxt.hook("schema:extend", (schemas) => {
-      schemas.push({
-        appConfig: {
-          vercelAnalytics: {},
-        },
-      });
-    });
   },
 });
+
+type Options = NonNullable<Parameters<typeof inject>[0]>;
+
+declare module "@nuxt/schema" {
+  interface CustomAppConfig {
+    vercelAnalytics?: {
+      /**
+       * Override the automatic environment detection.
+       * This option allows you to force a specific environment for the package.
+       * @default 'auto'
+       */
+      mode?: Options["mode"];
+
+      /**
+       * You'll see all analytics events in the browser's console with the debug mode.
+       * This option is automatically enabled if the NODE_ENV environment variable is available and is either development or test.
+       */
+      debug?: Options["debug"];
+
+      /**
+       * With the beforeSend option, you can modify the event data before it's sent to Vercel.
+       * Returning null will ignore the event and no data will be sent.
+       */
+      beforeSend?: Options["beforeSend"];
+    };
+  }
+}
